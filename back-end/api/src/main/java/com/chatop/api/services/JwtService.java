6508617ae3d8a4ae2efd458e4606e771f08b7ae1
@clemcir <clementcirou22@gmail.com>
2026-03-20 18:09:08 +0100

    refacto : set one hour expiration token

--- back-end/api/src/main/java/com/chatop/api/services/JwtService.java
+++ back-end/api/src/main/java/com/chatop/api/services/JwtService.java
@@ -19,7 +19,7 @@ public class JwtService {
         return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)) // 24 heures
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 de temps d'expiration
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
