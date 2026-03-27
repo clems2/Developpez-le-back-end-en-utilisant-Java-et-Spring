115fcccfa1e7148b9c2c7d43b320dfe2bc00131e
@clemcir <clementcirou22@gmail.com>
2026-03-27 11:55:23 +0100

    feat : Implemented Controller, and service for rental getById (getOne)

--- back-end/api/src/main/java/com/chatop/api/controllers/RentalController.java
+++ back-end/api/src/main/java/com/chatop/api/controllers/RentalController.java
@@ -1,10 +1,12 @@
 package com.chatop.api.controllers;
 
+import com.chatop.api.dto.RentalDto;
 import com.chatop.api.dto.RentalsResponse;
 import com.chatop.api.services.RentalService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +19,10 @@ public class RentalController {
     @GetMapping
     public ResponseEntity<RentalsResponse> getAll() {
         return ResponseEntity.ok(rentalService.getAllRentals());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RentalDto> getOne(@PathVariable Integer id) {
+        return ResponseEntity.ok(rentalService.getRentalById(id));
     }
 }
--- back-end/api/src/main/java/com/chatop/api/services/RentalService.java
+++ back-end/api/src/main/java/com/chatop/api/services/RentalService.java
@@ -2,6 +2,7 @@ package com.chatop.api.services;
 
 import com.chatop.api.dto.RentalDto;
 import com.chatop.api.dto.RentalsResponse;
+import com.chatop.api.models.Rental;
 import com.chatop.api.repositories.RentalRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -30,5 +31,26 @@ public class RentalService {
                 .collect(Collectors.toList());
 
         return new RentalsResponse(rentals);
+    }
+
+    public RentalDto getRentalById(Integer id) {
+        Rental rental = rentalRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Rental not found"));
+        return mapToDto(rental);
+    }
+
+    // Petite méthode utilitaire pour éviter la répétition du mapping
+    private RentalDto mapToDto(Rental rental) {
+        return RentalDto.builder()
+                .id(rental.getId())
+                .name(rental.getName())
+                .surface(rental.getSurface())
+                .price(rental.getPrice())
+                .picture(rental.getPicture())
+                .description(rental.getDescription())
+                .owner_id(rental.getOwner().getId())
+                .created_at(rental.getCreatedAt())
+                .updated_at(rental.getUpdatedAt())
+                .build();
     }
 }
