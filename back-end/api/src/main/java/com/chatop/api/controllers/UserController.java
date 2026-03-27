915661733b5f770d97c7a1758a3f507aca50f050
@clemcir <clementcirou22@gmail.com>
2026-03-27 16:17:25 +0100

    feat : Implemented getUser with id in path variable

---
+++ back-end/api/src/main/java/com/chatop/api/controllers/UserController.java
@@ -0,0 +1,23 @@
+package com.chatop.api.controllers;
+
+import com.chatop.api.dto.UserResponse;
+import com.chatop.api.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUser(@PathVariable("id") Integer id) {
+        System.out.println("getUser");
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+}
